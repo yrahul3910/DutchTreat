a7654c4266de071fc8e892f6efa88e22e5945812
Rahul Yedida <yrahul3910@gmail.com>
2018-01-04 23:55:53 +0530

    Serve static files, search for default files.

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -20,10 +20,8 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
