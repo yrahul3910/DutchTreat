2b4083d0bfe87a44bd83dd7b0769e9d455f1931c
Rahul Yedida <yrahul3910@gmail.com>
2018-01-05 19:22:33 +0530

    Added JavaScript

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,11 @@
+﻿console.log("Hello, Pluralsight");
+
+let form = document.getElementById("theForm");
+form.hidden = true;
+
+let btn = document.getElementById("buyButton");
+btn.addEventListener("click", (e) => {
+    console.log("Buying Button");
+});
+
+let productInfo = document.getElementsByClassName("product-props");
