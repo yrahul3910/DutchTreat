192f58b0afa9aee6522ed5dcca5921d417fa6536
Rahul Yedida <yrahul3910@gmail.com>
2018-01-11 00:20:43 +0530

    Created database using EF Core tooling

--- DutchTreat/Program.cs
+++ DutchTreat/Program.cs
@@ -19,7 +19,17 @@ namespace DutchTreat
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
                 .UseStartup<Startup>()
                 .Build();
+
+        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            // Remove the default confiuration options
+            builder.Sources.Clear();
+
+            builder.AddJsonFile("config.json", false, true)
+                   .AddEnvironmentVariables();
+        }
     }
 }
